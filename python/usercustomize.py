c1793bf8bcdb69fa4a3624d8dc43d66d16182bbd
len <l.yoshida.132@gmail.com>
2020-06-12 20:49:51 +0900

    remove comment

--- python/usercustomize.py
+++ python/usercustomize.py
@@ -6,15 +6,13 @@ import traceback
 IGNORE = {'NameError',
         'ModuleNotFoundError',
         'KeyboardInterrupt',
-        # 'SyntaxError',
+        'SyntaxError',
         'BdbQuit',
         }
 
 
 def debug_rescue(_type, _value, _traceback):
     traceback.print_exception(_type, _value, _traceback)
-    # for d in dir(_value):
-    #     print(getattr(_value, d))
     if _value.__class__.__name__ not in IGNORE:
         pdb.pm()
 
